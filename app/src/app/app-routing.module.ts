cee2e3d950cfdffe0ee2649938134e42060113d8
Thijs Wouters <r0793167@student.thomasmore.be>
2021-11-10 13:51:53 +0100

    Setup

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TaskComponent } from './task/task.component';
+import { TasklistComponent } from './tasklist/tasklist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'tasklist', component: TasklistComponent },
+  { path: 'task', component: TaskComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
