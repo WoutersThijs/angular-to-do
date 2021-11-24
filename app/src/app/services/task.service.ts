db5dd227066dea63e6e5569c7613fb094e0659f9
Thijs Wouters <r0793167@student.thomasmore.be>
2021-11-24 16:40:45 +0100

    Task service expanded

--- app/src/app/services/task.service.ts
+++ app/src/app/services/task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Task } from '../interfaces/task';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, timer } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
@@ -17,6 +17,24 @@ export class TaskService {
   }
 
   getTasksByList(listID: number): Observable<Task[]> {
-    return timer(1, 3000).pipe(switchMap(() => this.httpClient.get<Task[]>("http://localhost:3000/tasks?list_id=" + listID.toString())));
+    return timer(1, 3000).pipe(switchMap(() => this.httpClient.get<Task[]>("http://localhost:3000/tasks?list_id=" + listID)));
+  }
+
+  getTasksByID(taskID: number): Observable<Task[]> {
+    return timer(1, 3000).pipe(switchMap(() => this.httpClient.get<Task[]>("http://localhost:3000/tasks/" + taskID)));
+  }
+
+  postTask(task: Task): Observable<Task> {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+
+    return this.httpClient.post<Task>("http://localhost:3000/tasks", task, {headers: headers});
+  }
+
+  putTask(taskID: number, task: Task): Observable<Task> {
+    let headers = new HttpHeaders();
+    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
+
+    return this.httpClient.put<Task>("http://localhost:3000/tasks/" + taskID, task, {headers: headers});
   }
 }
